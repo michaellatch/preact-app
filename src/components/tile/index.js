604f73486fcbd1811e30a91c7e0fc50eaeef9adb
michael braham latch <mike@closehero.com>
2017-03-21 10:52:57 -0700

    Cleaned up Header styling

---
+++ src/components/tile/index.js
@@ -0,0 +1,14 @@
+import { h, Component } from 'preact';
+import style from './style';
+
+export default class Tile extends Component {
+  render() {
+    return (
+      <div class={style.home}>
+        <h1>Articles</h1>
+        <h2>Fluffy Dogs</h2>
+        <h2>17 Dogs Who Fail at Being Dogs</h2> 
+      </div>
+    );
+  }
+}
